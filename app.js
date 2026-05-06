59cb4968bf9fde437ea9e34bb36c527f7695c1ed
Aviral Mishra <avi.avimishra.com@gmail.com>
2026-05-06 10:12:53 +0530

    01-folder

---
+++ app.js
@@ -0,0 +1,4 @@
+const user = 'sarthak';
+export const skills = ['react', 'backend', 'ui/ux'];
+
+export default user;;
---
+++ script.jsx
@@ -0,0 +1,4 @@
+import name from './app.js';
+import user2 from './test.js'
+
+console.log(name, user2);
