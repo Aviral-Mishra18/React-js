fcfedc8e6e17b2260d46de0e2e4fd7813460df75
Aviral Mishra <avi.avimishra.com@gmail.com>
2026-05-13 00:58:06 +0530

    main.jsx

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,21 +1,7 @@
 import React from 'react'
-import Card from './components/card'
-
 const App = () => {
   return (
-    <div>
-      <div className='card'>
-        <h1>Aviral Mishra</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, ipsa.</p>
-      </div>
-      {Card()}
-
-    </div>
-
-
+    <div>App</div>
   )
 }
 export default App
-
-
-
